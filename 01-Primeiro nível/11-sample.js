be9ef9e712545f9e64727a96066f6e23ee50f173
Rodrigo <cabezas31@edu.unifor.br>
2024-11-11 17:42:11 -0300

    Arquivo corrigido

--- 01-Primeiro nível/11-sample.js
+++ 01-Primeiro nível/11-sample.js
@@ -1,4 +1,4 @@
-class z {
+class Usuario {
     constructor(a, b, c, d, e, f, g, h) {
         this.a = a;
         this.b = b;
@@ -10,7 +10,7 @@ class z {
         this.h = h;
     }
 
-    x() {
+    verificarAtividade() {
         if (this.f) {
             console.log("Ativo");
         } else {
@@ -18,7 +18,7 @@ class z {
         }
     }
 
-    y() {
+    verificarAdmnistrador() {
         if (this.c === 'admin') {
             console.log("Admin pode acessar todas as áreas.");
         } else {
@@ -28,19 +28,19 @@ class z {
 }
 
 // Deixe a função clara para que seja possível de compreender que ela verifica o número de usuários com a role de admin
-function t(users) {
-    let c = 0;
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].c === 'admin') {
-            c++;
+function contadorDeAdmnistradores(usuarios) {
+    let contador = 0;
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].contador === 'admin') {
+            contador++;
         }
     }
-    return c;
+    return contador;
 }
 
 // Deixe a função clara para que seja possível de compreender que ela verifica se o número de tentativas de login é excessivo
-function l(a, b) {
-    if (a.h > b) {
+function login(tentativas, limiteTentativas) {
+    if (tentativas.h > limiteTentativas) {
         console.log("Tentativas de login excessivas.");
     } else {
         console.log("Tentativas de login sob controle.");
@@ -48,38 +48,38 @@ function l(a, b) {
 }
 
 // Deixe a função clara para que seja possível de compreender que ela verifica qual usuário logou mais recentemente
-function c(u1, u2) {
-    if (u1.e > u2.e) {
-        return u1.a + " logou mais recentemente.";
+function ordemUsuarios(usuario1, usuario2) {
+    if (usuario1.e > usuario2.e) {
+        return usuario1.a + " logou mais recentemente.";
     } else {
-        return u2.a + " logou mais recentemente.";
+        return usuario2.a + " logou mais recentemente.";
     }
 }
 
 // Deixe a função clara para que seja possível de compreender que ela verifica se o usuário foi criado recentemente
-function r(user) {
-    let now = new Date();
-    if (now - user.d < 31536000000) {
+function usuarioRecente(usuario) {
+    let dataAtual = new Date();
+    if (dataAtual - usuario.d < 31536000000) {
         console.log("Usuário criado recentemente.");
     } else {
         console.log("Usuário antigo.");
     }
 }
 
-let usrs = [
-    new z("Carlos", 25, 'admin', new Date(2023, 1, 15), new Date(2024, 8, 1), true, 100, 2),
-    new z("Ana", 30, 'guest', new Date(2020, 4, 22), new Date(2024, 7, 31), true, 50, 3),
-    new z("José", 29, 'admin', new Date(2022, 10, 5), new Date(2024, 6, 10), false, 200, 5),
-    new z("Maria", 35, 'guest', new Date(2021, 2, 10), new Date(2023, 12, 25), false, 80, 7)
+let usuarios = [
+    new Usuario("Carlos", 25, 'admin', new Date(2023, 1, 15), new Date(2024, 8, 1), true, 100, 2),
+    new Usuario("Ana", 30, 'guest', new Date(2020, 4, 22), new Date(2024, 7, 31), true, 50, 3),
+    new Usuario("José", 29, 'admin', new Date(2022, 10, 5), new Date(2024, 6, 10), false, 200, 5),
+    new Usuario("Maria", 35, 'guest', new Date(2021, 2, 10), new Date(2023, 12, 25), false, 80, 7)
 ];
 
-console.log("Usuários admin: " + t(usrs));
+console.log("Usuários admin: " + contadorDeAdmnistradores(usuarios));
 
-usrs[0].x();
-usrs[1].y();
+usuarios[0].verificarAtividade();
+usuarios[1].verificarAdmnistrador();
 
-l(usrs[2], 4);
+login(usuarios[2], 4);
 
-console.log(c(usrs[0], usrs[3]));
+console.log(ordemUsuarios(usuarios[0], usuarios[3]));
 
-r(usrs[3]);
+usuarioRecente(usuarios[3]);
